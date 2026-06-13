fb6c0d5961ce6bd9c85a386c231c737bf2964486
Joseph Kummari <josephkummari939@gmail.com>
2026-06-13 18:36:07 +0530

    Move database credentials to environment variables

--- python_loader.py
+++ python_loader.py
@@ -1,25 +1,45 @@
+import os
 import pandas as pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
 print("Starting script...")
 
+# Load environment variables
+load_dotenv()
+
+# Database credentials
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
 # Create engine
 engine = create_engine(
-    "postgresql+psycopg2://postgres:pjar2005@localhost:5432/ipl_analysis"
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 
 print("Engine created!")
 
 # Load CSVs
-matches = pd.read_csv("data/matchesss_cleaned.csv")
-deliveries = pd.read_csv("data/deliveriesss_cleaned.csv")
+matches = pd.read_csv(
+    "data/matchesss_cleaned.csv",
+    low_memory=False
+)
+
+deliveries = pd.read_csv(
+    "data/deliveriesss_cleaned.csv",
+    low_memory=False
+)
 
 print("CSV files loaded!")
+print("Matches Shape:", matches.shape)
+print("Deliveries Shape:", deliveries.shape)
 
 # Test connection
 with engine.connect() as conn:
     print("Database connection successful!")
-
 
 # Load matches
 matches.to_sql(
